e8c6a47296f5b9a3524a78e76ebe69e15cea3eff
Croppedeyebrow <142734435+croppedeyebrow@users.noreply.github.com>
2025-01-29 21:21:37 +0900

    api키 발급 언제되냐~~

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 #Python 활용 날씨 API 불러오기.
+
+##OPENWEATHER API 활용.
--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print("Hello, World!")
+import requests
+import json
+
+city = "Seoul"
+apikey = ""
+lang = "ko"
+
+api = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={apikey}&lang={lang}&units=metric"
+
+result = requests.get(api)
+data = json.loads(result.text)
+
+if result.status_code != 200:
+    print(f"에러 발생: {data.get('message', '알 수 없는 에러')}")
+else:
+    print(data)
+
+	 
+
